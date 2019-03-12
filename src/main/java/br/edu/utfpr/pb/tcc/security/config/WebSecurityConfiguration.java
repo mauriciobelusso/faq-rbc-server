abe4e3364c5ab0d4d175c1a07c5a7f1f3b180c3b
Mauricio Belusso <mauriciobelusso@gmail.com>
2019-03-12 01:33:48 -0300

    Path inexistente na API

--- src/main/java/br/edu/utfpr/pb/tcc/security/config/WebSecurityConfiguration.java
+++ src/main/java/br/edu/utfpr/pb/tcc/security/config/WebSecurityConfiguration.java
@@ -68,8 +68,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
 				.antMatchers("/auth/**").permitAll()
 				.antMatchers("/usuario/**").permitAll()
 				.antMatchers("/usuario/**").hasAnyRole("USER,ADMIN")
-				.antMatchers("/usuario/register/**").permitAll()
-				.antMatchers("/usuario/register/**").hasAnyRole("USER,ADMIN")
 				.anyRequest().authenticated();
 		http.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
 	}
