1ee2b992d825339bcccb19badb48d41a003be588
mauricio.belusso <mauricio.belusso@senior.com.br>
2021-05-04 00:07:53 -0300

    Trocado usuário para admin/admin

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,7 @@
 INSERT INTO permissao (permissao) values('ROLE_USER');
 INSERT INTO permissao (permissao) values('ROLE_ADMIN');
 
-INSERT INTO usuario(nome, username, password) VALUES ('Administrador', 'admin','$2a$10$.PVIfB07x.SfMYTcToxL0.yxcLWU0GbS2NUO1W1QAvqMm/TsFhVem');
+INSERT INTO usuario(nome, username, password) VALUES ('Administrador', 'admin','$2a$10$.9IVz3RIOXndP/CVgKeIDOEPlR35SmIPhZBTWD1qOzjiOM/dBKe1i');
 INSERT INTO usuario(nome, username, password) VALUES ('Teste', 'teste','$2a$10$.PVIfB07x.SfMYTcToxL0.yxcLWU0GbS2NUO1W1QAvqMm/TsFhVem');
 
 INSERT INTO usuario_permissoes(Usuario_id, Permissoes_id) VALUES (1, 1);
