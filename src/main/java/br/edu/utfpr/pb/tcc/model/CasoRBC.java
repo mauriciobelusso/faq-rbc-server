c2fbc33d300bd311ef83dde77d117e36e446dd0c
mauricio.belusso <mauricio.belusso@senior.com.br>
2021-05-04 01:59:54 -0300

    Implementada classe interna para facilitar o cálculo

--- src/main/java/br/edu/utfpr/pb/tcc/model/CasoRBC.java
+++ src/main/java/br/edu/utfpr/pb/tcc/model/CasoRBC.java
@@ -1,5 +1,6 @@
 package br.edu.utfpr.pb.tcc.model;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,13 +13,19 @@ public class CasoRBC {
 	public void processar(List<Caso> casos, Caso novoCaso) {
 		this.casos = casos;
         List<CasoSimilaridade> casosSimilares = new ArrayList<>();
-        
-        this.casos.forEach(
+                
+        this.casos
+        	.stream()
+	    	.sorted(
+	    			(caso1, caso2) -> 
+	    				caso1.getId() >= caso2.getId() ? 1 : -1
+	    	)
+        	.forEach(
         		caso -> 
         		casosSimilares.add(
         				similaridade(novoCaso, caso)
         		)
-        );
+        	);
         
         this.casosSimilares.addAll(
 	    	casosSimilares
@@ -31,27 +38,26 @@ public class CasoRBC {
         );
 	}
 	
-	private Double similaridadeNumerica(Double a1, Double a2, Double max, Double min) {
-		if (max-min != 0D) {
-	        return (1.0 - (Math.abs(a2 - a1) / (max - min)));			
-		} else {
-			return 0D;
-		}
-    }
+//	private Double similaridadeNumerica(Double a1, Double a2, Double max, Double min) {
+//		if (max-min != 0D) {
+//	        return (1.0 - (Math.abs(a2 - a1) / (max - min)));			
+//		} else {
+//			return 0D;
+//		}
+//    }
 	
 	private Double similaridadeSimbolos(String a1, String a2) {
 		return (a1.equalsIgnoreCase(a2) ? 1D : 0D);
 	}
 	
-	private Double somaSimilaridade;
-	private CasoSimilaridade similaridade(Caso novoCaso, Caso caso) {
-        somaSimilaridade = 0D;
+	private CasoSimilaridade similaridade(Caso novoCaso, Caso caso) {        
+		SomaSimilaridade somaSimilaridade = new SomaSimilaridade();
         
         caso.getAtributos().forEach(atributo -> {
         	Atributo atributoNovoCaso = novoCaso.getAtributo(atributo.getDescricao());
         	
         	if (!atributoNovoCaso.getDescricao().isEmpty()) {
-        		somaSimilaridade += similaridadeSimbolos(atributoNovoCaso.getDescricao(), atributo.getDescricao()) * atributo.getPeso();
+        		somaSimilaridade.add(similaridadeSimbolos(atributoNovoCaso.getDescricao(), atributo.getDescricao()) * atributo.getPeso());
 //		        somaSimilaridade +=
 //		        		similaridadeNumerica(
 //		        				atributo.getPeso(),
@@ -67,7 +73,7 @@ public class CasoRBC {
         peso.setAtributos(caso.getAtributos());
 
         if (peso.getTotal() > 0) {
-            return new CasoSimilaridade(caso, somaSimilaridade / peso.getTotal());	
+            return new CasoSimilaridade(caso, somaSimilaridade.value() / peso.getTotal());	
         } else {
         	return new CasoSimilaridade(caso, 0D);
         }
@@ -75,5 +81,17 @@ public class CasoRBC {
 
 	public List<CasoSimilaridade> getCasosSimilares() {
 		return casosSimilares;
+	}
+	
+	class SomaSimilaridade {
+		private Double soma = 0D;
+		
+		public void add(Double value) {
+			soma += value;
+		}
+		
+		public Double value() {
+			return soma;
+		}
 	}
 }
