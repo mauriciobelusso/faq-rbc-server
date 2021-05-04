1dcf7b0e1c35e775ae80a6d57b3b50882d89ee62
mauricio.belusso <mauricio.belusso@senior.com.br>
2021-05-03 23:25:36 -0300

    facilitando login

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,8 @@
 INSERT INTO permissao (permissao) values('ROLE_USER');
 INSERT INTO permissao (permissao) values('ROLE_ADMIN');
 
-INSERT INTO usuario(nome, username, password) VALUES ('Administrador', 'admin@admin.com','$2a$10$.PVIfB07x.SfMYTcToxL0.yxcLWU0GbS2NUO1W1QAvqMm/TsFhVem');
-INSERT INTO usuario(nome, username, password) VALUES ('Teste', 'teste@teste.com','$2a$10$.PVIfB07x.SfMYTcToxL0.yxcLWU0GbS2NUO1W1QAvqMm/TsFhVem');
+INSERT INTO usuario(nome, username, password) VALUES ('Administrador', 'admin','$2a$10$.PVIfB07x.SfMYTcToxL0.yxcLWU0GbS2NUO1W1QAvqMm/TsFhVem');
+INSERT INTO usuario(nome, username, password) VALUES ('Teste', 'teste','$2a$10$.PVIfB07x.SfMYTcToxL0.yxcLWU0GbS2NUO1W1QAvqMm/TsFhVem');
 
 INSERT INTO usuario_permissoes(Usuario_id, Permissoes_id) VALUES (1, 1);
 INSERT INTO usuario_permissoes(Usuario_id, Permissoes_id) VALUES (1, 2);
